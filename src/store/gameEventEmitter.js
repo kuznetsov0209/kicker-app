61b64b6b1da7659839875d01e84451fe777b1fb6
Алексей Кузнецов <kuznetsov0209@gmail.com>
2018-10-02 00:39:49 +0400

    Add game's events emitter

---
+++ src/store/gameEventEmitter.js
@@ -0,0 +1,37 @@
+function createGameEventEmitter() {
+  const eventListeners = [];
+
+  function* waitForEvent(type, payload = {}) {
+    while (true) {
+      const event = yield;
+      if (!type || event.type === type) {
+        return event;
+      }
+    }
+  }
+
+  class GameEventEmitter {
+    emit(type, payload) {
+      eventListeners.forEach(eventListener =>
+        eventListener.next({ type, payload })
+      );
+    }
+
+    addListener(generatorFunc) {
+      const generator = generatorFunc({ waitForEvent });
+      generator.next();
+      eventListeners.push(generator);
+    }
+  }
+
+  return new GameEventEmitter();
+}
+
+export default createGameEventEmitter();
+
+export const EVENT_GAME_STARTED = "EVENT_GAME_STARTED";
+export const EVENT_GAME_FINISHED = "EVENT_GAME_FINISHED";
+export const EVENT_SCORE_CHANGED = "EVENT_SCORE_CHANGED";
+export const EVENT_GOAL = "EVENT_GOAL";
+export const EVENT_OWN_GOAL = "EVENT_OWN_GOAL";
+export const EVENT_UNDO_GOAL = "EVENT_UNDO_GOAL";
